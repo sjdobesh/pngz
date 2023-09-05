dd4ff5b444f702fe6136ffd76a0018fd4b46086e
sjdobesh <samjdobesh@gmail.com>
2023-09-05 14:36:30 -0700

    best practices for include paths

--- test/test.c
+++ test/test.c
@@ -1,12 +1,12 @@
 #include "../src/spng.h"
 
 int main() {
-  spng s;
-  spng_load(&s, "./png/Catgun.png");
+  spng s = { .path = "./png/Catgun.png" };
+  spng_load(&s);
   printf("%u, %u\n", s.width, s.height);
   spng_print(s);
   spng_print_pixel(s.pixels[0][s.width - 1]);
-  spng_save(s, "./png/TEST.png");
+  spng_save(s);
   spng_print(s);
   spng_free(&s);
   spng_print(s);
