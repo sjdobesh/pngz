0a1626bdfa3b2b1f34dc3f2351e3217037bbdba7
sjdobesh <samjdobesh@gmail.com>
2023-09-14 00:56:54 -0700

    regrouping functions

--- src/spng.c
+++ src/spng.c
@@ -24,80 +24,6 @@
 #include <png.h>
 #include "spng.h"
 
-/* multithreaded filtering *--------------------------------------------------*/
-pthread_barrier_t thread_barrier;
-
-/**
- * thread functions that accesses rows of pixels.
- *
- * @param params thread parameter struct 
- * @return void*, works via ptr reference 
- */
-void* spng_filter_threadfn(void* params) {
-  /* read in params */
-  struct thread_parameters* tp = (thread_parameters*) params;
-  /* apply the filter here for subsection of rows */
-  for (unsigned i = tp->row_start; i < tp->row_stop; i++) {
-    for (unsigned j = 0; j < tp->i_image->width; j++) {
-      tp->o_buf[i][j] = tp->filter_function(tp->i_image->pixels[i][j]);
-    }
-  }
-  /* halt threads */
-  pthread_barrier_wait(&thread_barrier);
-  return NULL;
-}
-
-/**
- * dispatches multithreaded filter.
- *
- * @param s spng reference to filter 
- * @param filter some filter function taking and returning a pixel 
- * @return exit code, setting errno on failure
- */
-int spng_filter_threaded(spng* s, pixel (*filter)(pixel), unsigned thread_count) {
-
-  /* make an empty spng */
-  pixel** output = spng_alloc_pixels(s->rows, s->cols);
-
-  /* thread containers and barrier */
-  pthread_t* threads = malloc(sizeof(pthread_t) * thread_count);
-  thread_parameters* tps = malloc(sizeof(thread_parameters) * thread_count);
-
-  /* set barrier */
-  pthread_barrier_init(&thread_barrier, NULL, thread_count);
-
-  /* calc how much to do */
-  unsigned divisions = s->height / thread_count;
-
-  /* launch threads */
-  for (unsigned i = 0; i < thread_count; i++) {
-    /* initialize thread params here */
-    tps[i] = (thread_parameters) {
-      .i_image = s, 
-      .o_buf = output,
-      .filter_function = filter,
-      .row_start = i * divisions,
-      .row_stop = (i + 1) * divisions > s->height ? s->height : (i+1) * divisions,
-      .thread_id = i
-    };
-    if (pthread_create(&threads[i], NULL, spng_filter_threadfn, (void*)&tps[i])) {
-      fprintf(stderr, "[ERROR] unable to create thread %d\n", i);
-    }
-  }
-
-  /* clean up threads */
-  for (unsigned i = 0; i < thread_count; i++) {
-    pthread_join(threads[i], NULL);
-  }
-  pthread_barrier_destroy(&thread_barrier);
-  free(threads);
-  free(tps);
-
-  /* free input and swap */
-  spng_free_pixels(s->pixels, s->rows);
-  s->pixels = output;
-  return 0;
-}
 
 /* alloc & free *-------------------------------------------------------------*/
 
@@ -481,6 +407,81 @@ int spng_filter(spng* s, pixel(*filter)(pixel)) {
       output[row][col] = filter(s->pixels[row][col]);
     }
   }
+  spng_free_pixels(s->pixels, s->rows);
+  s->pixels = output;
+  return 0;
+}
+
+/* multithreaded filtering *--------------------------------------------------*/
+pthread_barrier_t thread_barrier;
+
+/**
+ * thread functions that accesses rows of pixels.
+ *
+ * @param params thread parameter struct 
+ * @return void*, works via ptr reference 
+ */
+void* spng_filter_threadfn(void* params) {
+  /* read in params */
+  struct thread_parameters* tp = (thread_parameters*) params;
+  /* apply the filter here for subsection of rows */
+  for (unsigned i = tp->row_start; i < tp->row_stop; i++) {
+    for (unsigned j = 0; j < tp->i_image->width; j++) {
+      tp->o_buf[i][j] = tp->filter_function(tp->i_image->pixels[i][j]);
+    }
+  }
+  /* halt threads */
+  pthread_barrier_wait(&thread_barrier);
+  return NULL;
+}
+
+/**
+ * dispatches multithreaded filter.
+ *
+ * @param s spng reference to filter 
+ * @param filter some filter function taking and returning a pixel 
+ * @return exit code, setting errno on failure
+ */
+int spng_filter_threaded(spng* s, pixel (*filter)(pixel), unsigned thread_count) {
+
+  /* make an empty spng */
+  pixel** output = spng_alloc_pixels(s->rows, s->cols);
+
+  /* thread containers and barrier */
+  pthread_t* threads = malloc(sizeof(pthread_t) * thread_count);
+  thread_parameters* tps = malloc(sizeof(thread_parameters) * thread_count);
+
+  /* set barrier */
+  pthread_barrier_init(&thread_barrier, NULL, thread_count);
+
+  /* calc how much to do */
+  unsigned divisions = s->height / thread_count;
+
+  /* launch threads */
+  for (unsigned i = 0; i < thread_count; i++) {
+    /* initialize thread params here */
+    tps[i] = (thread_parameters) {
+      .i_image = s, 
+      .o_buf = output,
+      .filter_function = filter,
+      .row_start = i * divisions,
+      .row_stop = (i + 1) * divisions > s->height ? s->height : (i+1) * divisions,
+      .thread_id = i
+    };
+    if (pthread_create(&threads[i], NULL, spng_filter_threadfn, (void*)&tps[i])) {
+      fprintf(stderr, "[ERROR] unable to create thread %d\n", i);
+    }
+  }
+
+  /* clean up threads */
+  for (unsigned i = 0; i < thread_count; i++) {
+    pthread_join(threads[i], NULL);
+  }
+  pthread_barrier_destroy(&thread_barrier);
+  free(threads);
+  free(tps);
+
+  /* free input and swap */
   spng_free_pixels(s->pixels, s->rows);
   s->pixels = output;
   return 0;
