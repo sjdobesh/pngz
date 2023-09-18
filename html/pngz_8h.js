7069126f6dfd3074ee9e13dba145ef46fc71358b
sjdobesh <samjdobesh@gmail.com>
2023-09-17 23:20:42 -0700

    updating documentation and git ignore to ignore png folder

---
+++ html/pngz_8h.js
@@ -0,0 +1,22 @@
+var pngz_8h =
+[
+    [ "pixel", "structpixel.html", "structpixel" ],
+    [ "pngz", "structpngz.html", "structpngz" ],
+    [ "pixel", "pngz_8h.html#adbb58f7670e1dd0e03e1529819c3688a", null ],
+    [ "pngz", "pngz_8h.html#a6b702c1633021e4ecaa3926f463aa84c", null ],
+    [ "pngz_alloc_bytes", "pngz_8h.html#a3954d462ebff7dd0ef37211c3094d86b", null ],
+    [ "pngz_alloc_pixels", "pngz_8h.html#a6ded83ee3ef265fc8c5eb0ce1d9bd037", null ],
+    [ "pngz_free", "pngz_8h.html#a7ed0aa704996d95835e6bc80899ec379", null ],
+    [ "pngz_free_bytes", "pngz_8h.html#a22abfe1d65981b053fbc8f82e01b6bdd", null ],
+    [ "pngz_free_pixels", "pngz_8h.html#aa7732352c6246abe07bbd28c57fc1d40", null ],
+    [ "pngz_load", "pngz_8h.html#a165e7ec113e942ee7f248ff3bb5f6679", null ],
+    [ "pngz_load_from", "pngz_8h.html#a3fbec9b62b321d08ae248d4cf2b144e8", null ],
+    [ "pngz_pack_pixels", "pngz_8h.html#ae2fb5d2a3493c3c815e7587c129f1e6c", null ],
+    [ "pngz_print", "pngz_8h.html#ae40a6f0edff212aa57e11cf887b2ad70", null ],
+    [ "pngz_print_indent", "pngz_8h.html#a79e5b969faa23bd49fb7a144edd02c6d", null ],
+    [ "pngz_print_pixel", "pngz_8h.html#a90e2de54530d8531132d10c8541834d9", null ],
+    [ "pngz_print_pixel_indent", "pngz_8h.html#a4cfed75396997c3e8662ae070fc948f1", null ],
+    [ "pngz_save", "pngz_8h.html#a69b6605f5874d6f17b775bb416c74c67", null ],
+    [ "pngz_save_as", "pngz_8h.html#afbb5d1eb634f7ac22208d40cc84210b7", null ],
+    [ "pngz_unpack_pixels", "pngz_8h.html#a105db7e9aa2f8e03cc79e5360c53ac71", null ]
+];
---
+++ html/search/variables_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['width_61',['width',['../structpngz.html#af7d1200181d1a9f82e46dd106b1c7d85',1,'pngz']]]
+];
